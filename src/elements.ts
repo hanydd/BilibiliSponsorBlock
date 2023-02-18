1471481ee432e39ba87440abb1aadb580df0521a
Ajay <dev@ajay.app>
2023-02-18 03:23:56 -0500

    Add title node selector

--- src/elements.ts
+++ src/elements.ts
@@ -1,4 +1,8 @@
-export function getYouTubeTitleNode(): HTMLElement {
+export function getYouTubeTitleNodeSelector(): string {
     // New YouTube Title, YouTube, Mobile YouTube, Invidious
-    return document.querySelector("#title h1, .ytd-video-primary-info-renderer.title, .slim-video-information-title, #player-container + .h-box > h1") as HTMLElement;
+    return "#title h1, .ytd-video-primary-info-renderer.title, .slim-video-information-title, #player-container + .h-box > h1";
+}
+
+export function getYouTubeTitleNode(): HTMLElement {
+    return document.querySelector(getYouTubeTitleNodeSelector()) as HTMLElement;
 }
