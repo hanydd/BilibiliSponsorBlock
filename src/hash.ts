de75a90da9056884a7f180e35b6a60f4ff99db60
Ajay <dev@ajay.app>
2023-07-11 17:28:11 -0400

    Use background script for hashing if not available in page
    For Invidious instances on http

--- src/hash.ts
+++ src/hash.ts
@@ -3,6 +3,21 @@ export type HashedValue = string & { __hashBrand: unknown };
 export async function getHash<T extends string>(value: T, times = 5000): Promise<T & HashedValue> {
     if (times <= 0) return "" as T & HashedValue;
 
+    if (!("subtle" in crypto)) {
+        // Run in background script instead
+        return new Promise((resolve, reject) => chrome.runtime.sendMessage({
+            message: "getHash",
+            value,
+            times
+        }, (response) => {
+            if (response.error) {
+                reject(response.error);
+            } else {
+                resolve(response);
+            }
+        }));
+    }
+
     let hashHex: string = value;
     for (let i = 0; i < times; i++) {
         const hashBuffer = await crypto.subtle.digest('SHA-256', new TextEncoder().encode(hashHex).buffer);
