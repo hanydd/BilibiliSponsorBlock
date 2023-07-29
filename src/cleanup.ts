b3572eaeff9235c6d4f8bd8b89540c2c5686b925
Ajay <dev@ajay.app>
2023-07-29 01:41:40 -0400

    Add support for injecting scripts in manifest v2

--- src/cleanup.ts
+++ src/cleanup.ts
@@ -34,21 +34,51 @@ export function getCleanupStartMessage() {
 }
 
 export async function injectUpdatedScripts() {
-    for (const cs of chrome.runtime.getManifest().content_scripts!) {
-        for (const tab of await chrome.tabs.query({url: cs.matches})) {
-            if (cs.css && cs.css.length > 0) {
-                await chrome.scripting.insertCSS({
+    if (chrome.runtime.getManifest().manifest_version === 3) {
+        for (const cs of chrome.runtime.getManifest().content_scripts!) {
+            for (const tab of await chrome.tabs.query({url: cs.matches})) {
+                if (cs.css && cs.css.length > 0) {
+                    await chrome.scripting.insertCSS({
+                        target: {tabId: tab.id!},
+                        files: cs.css || [],
+                    })
+                }
+    
+                await chrome.scripting.executeScript({
                     target: {tabId: tab.id!},
-                    files: cs.css || [],
+                    files: cs.js || [],
+                    
+                    world: cs["world"] || "ISOLATED"
                 })
             }
-
-            await chrome.scripting.executeScript({
-                target: {tabId: tab.id!},
-                files: cs.js || [],
-                
-                world: cs["world"] || "ISOLATED"
-            })
         }
+    } else {
+        chrome.windows.getAll({
+            populate: true
+        }, (windows) => {
+            for (const window of windows) {
+                for (const tab of window.tabs) {
+                    for (const script of chrome.runtime.getManifest().content_scripts) {
+                        if (tab.url && script.matches.some((match) => 
+                                tab.url.match(match
+                                        .replace(/\//g, "\\/")
+                                        .replace(/\./g, "\\.")
+                                        .replace(/\*/g, ".*")))) {
+                            for (const file of script.js) {
+                                chrome.tabs.executeScript(tab.id, {
+                                    file
+                                });
+                            }
+
+                            for (const file of script.css) {
+                                chrome.tabs.insertCSS(tab.id, {
+                                    file
+                                });
+                            }
+                        }
+                    }
+                }
+            }
+        });
     }
 }
