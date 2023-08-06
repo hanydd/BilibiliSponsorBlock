9b82b3198fbc4787c6e12443b069c94e25d20232
Ajay <dev@ajay.app>
2023-08-06 02:24:07 -0400

    Support extra scripts in inject scripts method

--- src/cleanup.ts
+++ src/cleanup.ts
@@ -33,9 +33,16 @@ export function getCleanupStartMessage() {
     return "cleanup-start"
 }
 
-export async function injectUpdatedScripts() {
+export interface InjectedScript {
+    matches?: string[];
+    js?: string[];
+    css?: string[];
+}
+
+export async function injectUpdatedScripts(extraScripts: InjectedScript[] = []) {
+    const scripts = extraScripts.concat(chrome.runtime.getManifest().content_scripts || []);
     if (chrome.runtime.getManifest().manifest_version === 3) {
-        for (const cs of chrome.runtime.getManifest().content_scripts!) {
+        for (const cs of scripts) {
             for (const tab of await chrome.tabs.query({url: cs.matches})) {
                 if (cs.css && cs.css.length > 0) {
                     await chrome.scripting.insertCSS({
@@ -59,28 +66,25 @@ export async function injectUpdatedScripts() {
             for (const window of windows) {
                 if (window.tabs) {
                     for (const tab of window.tabs) {
-                        const scripts = chrome.runtime.getManifest().content_scripts;
-                        if (scripts) {
-                            for (const script of scripts) {
-                                if (tab.url && script.matches?.some?.((match) => 
-                                        tab.url!.match(match
-                                                .replace(/\//g, "\\/")
-                                                .replace(/\./g, "\\.")
-                                                .replace(/\*/g, ".*")))) {
-                                    if (script.js) {
-                                        for (const file of script.js) {
-                                            void chrome.tabs.executeScript(tab.id!, {
-                                                file
-                                            });
-                                        }
+                        for (const script of scripts) {
+                            if (tab.url && script.matches?.some?.((match) => 
+                                    tab.url!.match(match
+                                            .replace(/\//g, "\\/")
+                                            .replace(/\./g, "\\.")
+                                            .replace(/\*/g, ".*")))) {
+                                if (script.js) {
+                                    for (const file of script.js) {
+                                        void chrome.tabs.executeScript(tab.id!, {
+                                            file
+                                        });
                                     }
-        
-                                    if (script.css) {
-                                        for (const file of script.css) {
-                                            void chrome.tabs.insertCSS(tab.id!, {
-                                                file
-                                            });
-                                        }
+                                }
+    
+                                if (script.css) {
+                                    for (const file of script.css) {
+                                        void chrome.tabs.insertCSS(tab.id!, {
+                                            file
+                                        });
                                     }
                                 }
                             }
