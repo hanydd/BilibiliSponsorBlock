be6b063b6f277537be396c1c09af8cf3805c5901
HanYaodong <hanyaodongcn@gmail.com>
2024-02-25 17:50:05 +0800

    Update excuteScript to scripting api

--- src/cleanup.ts
+++ src/cleanup.ts
@@ -15,7 +15,7 @@ export function setupCleanupListener() {
     });
 
     window.addEventListener("message", (message) => {
-        if (message.data?.source 
+        if (message.data?.source
                 && message.data.source === source
                 && message.data.message === getCleanupStartMessage()
                 && performance.now() - started > 5000) {
@@ -52,11 +52,11 @@ export async function injectUpdatedScripts(extraScripts: InjectedScript[] = []) 
                         files: cs.css || [],
                     })
                 }
-    
+
                 await chromeP.scripting.executeScript({
                     target: {tabId: tab.id!},
                     files: cs.js || [],
-                    
+
                     world: cs["world"] || "ISOLATED"
                 })
             }
@@ -69,19 +69,18 @@ export async function injectUpdatedScripts(extraScripts: InjectedScript[] = []) 
                 if (window.tabs) {
                     for (const tab of window.tabs) {
                         for (const script of scripts) {
-                            if (tab.url && script.matches?.some?.((match) => 
+                            if (tab.url && script.matches?.some?.((match) =>
                                     tab.url!.match(match
                                             .replace(/\//g, "\\/")
                                             .replace(/\./g, "\\.")
                                             .replace(/\*/g, ".*")))) {
                                 if (script.js) {
-                                    for (const file of script.js) {
-                                        void chrome.tabs.executeScript(tab.id!, {
-                                            file
-                                        });
-                                    }
+                                    void chromeP.scripting.executeScript({
+                                        target: { tabId: tab.id! },
+                                        files: script.js
+                                    });
                                 }
-    
+
                                 if (script.css) {
                                     for (const file of script.css) {
                                         void chrome.tabs.insertCSS(tab.id!, {
