f163fee4ecb39cd5a644fe3c36ea6990957b2e0f
HanYaodong <hanyaodongcn@gmail.com>
2024-04-13 16:03:28 +0800

    Remove auto generate invidous sites

--- ci/prettify.ts
+++
@@ -1,6 +0,0 @@
-import { writeFile } from 'fs';
-
-import * as license from "../oss-attribution/licenseInfos.json";
-
-const result = JSON.stringify(license, null, 2);
-writeFile("../oss-attribution/licenseInfos.json", result, err => { if (err) return console.log(err) } );
