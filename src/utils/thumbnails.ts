cb46b21ba2749244635e5e67a7d6514c348e6e47
HanYaodong <hanyaodongcn@gmail.com>
2024-04-27 10:38:55 +0800

    Fix the insertion anchor of thumbnail svg

--- src/utils/thumbnails.ts
+++ src/utils/thumbnails.ts
@@ -111,7 +111,7 @@ function insertSBIconDefinition() {
     </g>
   </defs>
 </svg>`;
-    document.body.appendChild(container.children[0]);
+    document.body.querySelector('img#wxwork-share-pic').after(container.children[0]);
 }
 
 export function setupThumbnailListener(): void {
