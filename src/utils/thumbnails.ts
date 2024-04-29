4d6cf5c0d58f329e883f036614f44e8fc6126b8f
HanYaodong <hanyaodongcn@gmail.com>
2024-04-29 17:51:21 +0800

    Revert insert svg anchor

--- src/utils/thumbnails.ts
+++ src/utils/thumbnails.ts
@@ -111,7 +111,7 @@ function insertSBIconDefinition() {
     </g>
   </defs>
 </svg>`;
-    document.body.querySelector('div#app')?.appendChild(container.children[0]);
+    document.body.appendChild(container.children[0]);
 }
 
 export function setupThumbnailListener(): void {
