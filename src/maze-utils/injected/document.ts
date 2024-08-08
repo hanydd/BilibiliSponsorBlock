366cb6e372a16484697c5e5400e67ea0a7dbaab1
HanYaodong <hanyaodongcn@gmail.com>
2024-08-08 13:01:45 +0800

    Remove injected script version handler

--- src/maze-utils/injected/document.ts
+++ src/maze-utils/injected/document.ts
@@ -1,5 +1,3 @@
-import { version } from "../version.json";
-import { versionHigher } from "../versionHigher";
 import { resetLastArtworkSrc } from "./mediaSession";
 
 export const sourceId = "biliSponsorBlock";
@@ -17,33 +15,6 @@ function windowMessageListener(message: MessageEvent) {
     }
 }
 
-// TODO: rewrite with bilibili api.
 export function init(): void {
-    // Should it teardown an old copy of the script, to replace it if it is a newer version (two extensions installed at once)
-    const shouldTearDown =
-        document.querySelector("#sponsorblock-document-script")?.getAttribute?.("teardown") === "true";
-    const versionBetter = window["versionCB"] && (!window["versionCB"] || versionHigher(version, window["versionCB"]));
-    if (shouldTearDown || versionBetter) {
-        window["teardownCB"]?.();
-    } else if (window["versionCB"] && !versionHigher(version, window["versionCB"])) {
-        // Leave the other script be then
-        return;
-    }
-
-    window["versionCB"] = version;
-    window["teardownCB"] = teardown;
-
-    // For compatibility with older versions of the document script;
-    const fakeDocScript = document.createElement("div");
-    fakeDocScript.id = "sponsorblock-document-script";
-    fakeDocScript.setAttribute("version", version);
-    const head = document.head || document.documentElement;
-    head.appendChild(fakeDocScript);
-
     window.addEventListener("message", windowMessageListener);
-}
-
-function teardown() {
-    window.removeEventListener("message", windowMessageListener);
-    window["teardownCB"] = null;
 }
--- src/maze-utils/scriptInjector.ts
+++ src/maze-utils/scriptInjector.ts
@@ -1,21 +1,11 @@
-import { versionHigher } from "./versionHigher";
-import { version } from "./version.json";
-
 export function injectScript(src: string) {
     const docScript = document.createElement("script");
     docScript.id = "sponsorblock-document-script";
-    docScript.setAttribute("version", version);
     docScript.innerHTML = src;
 
     const head = document.head || document.documentElement;
     const existingScript = document.getElementById("sponsorblock-document-script");
-    const existingScriptVersion = existingScript?.getAttribute("version");
-    if (head && (!existingScript || versionHigher(version, existingScriptVersion ?? ""))) {
-        if (existingScript) {
-            docScript.setAttribute("teardown", "true");
-            existingScript.remove();
-        }
-
+    if (head && !existingScript) {
         head.appendChild(docScript);
     }
 }
