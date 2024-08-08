8c4fed02dd31e6c0685ec12b460bf2ebbdd01d36
HanYaodong <hanyaodongcn@gmail.com>
2024-08-08 11:05:52 +0800

    Remove safari and beta webpack config

--- webpack/webpack.manifest.js
+++ webpack/webpack.manifest.js
@@ -1,33 +1,32 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const webpack = require("webpack");
-const path = require('path');
-const { validate } = require('schema-utils');
-const fs = require('fs');
+const path = require("path");
+const { validate } = require("schema-utils");
+const fs = require("fs");
 
 const manifest = require("../manifest/manifest.json");
 const firefoxManifestExtra = require("../manifest/firefox-manifest-extra.json");
 const chromeManifestExtra = require("../manifest/chrome-manifest-extra.json");
-const safariManifestExtra = require("../manifest/safari-manifest-extra.json");
 
 // schema for options object
 const schema = {
-    type: 'object',
+    type: "object",
     properties: {
         browser: {
-            type: 'string'
+            type: "string",
         },
         pretty: {
-            type: 'boolean'
+            type: "boolean",
         },
         steam: {
-            type: 'string'
-        }
-    }
+            type: "string",
+        },
+    },
 };
 
 class BuildManifest {
-    constructor (options = {}) {
+    constructor(options = {}) {
         validate(schema, options, "Build Manifest Plugin");
 
         this.options = options;
@@ -40,26 +39,18 @@ class BuildManifest {
         // Add missing manifest elements
         if (this.options.browser.toLowerCase() === "firefox") {
             mergeObjects(manifest, firefoxManifestExtra);
-        } else if (this.options.browser.toLowerCase() === "chrome"
-                || this.options.browser.toLowerCase() === "chromium"
-                || this.options.browser.toLowerCase() === "edge") {
+        } else if (
+            this.options.browser.toLowerCase() === "chrome" ||
+            this.options.browser.toLowerCase() === "chromium" ||
+            this.options.browser.toLowerCase() === "edge"
+        ) {
             mergeObjects(manifest, chromeManifestExtra);
-        }  else if (this.options.browser.toLowerCase() === "safari") {
-            mergeObjects(manifest, safariManifestExtra);
-        }
-
-        if (this.options.stream === "beta") {
-            mergeObjects(manifest, betaManifestExtra);
-
-            if (this.options.browser.toLowerCase() === "firefox") {
-                mergeObjects(manifest, firefoxBetaManifestExtra);
-            }
         }
 
         let result = JSON.stringify(manifest);
         if (this.options.pretty) result = JSON.stringify(manifest, null, 2);
 
-        fs.mkdirSync(distFolder, {recursive: true});
+        fs.mkdirSync(distFolder, { recursive: true });
         fs.writeFileSync(distManifestFile, result);
     }
 }
@@ -69,7 +60,7 @@ function mergeObjects(object1, object2) {
         if (key in object1) {
             if (Array.isArray(object1[key])) {
                 object1[key] = object1[key].concat(object2[key]);
-            } else if (typeof object1[key] == 'object') {
+            } else if (typeof object1[key] == "object") {
                 mergeObjects(object1[key], object2[key]);
             } else {
                 object1[key] = object2[key];
