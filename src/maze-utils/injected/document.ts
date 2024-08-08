368940689032df6dbea864ed900124d6287f9074
HanYaodong <hanyaodongcn@gmail.com>
2024-08-08 13:05:44 +0800

    Remove deArrow related code

--- src/maze-utils/injected/document.ts
+++ src/maze-utils/injected/document.ts
@@ -1,14 +1,13 @@
-import { resetLastArtworkSrc } from "./mediaSession";
-
 export const sourceId = "biliSponsorBlock";
 const sendMessage = (message): void => {
     window.postMessage({ source: sourceId, ...message }, "/");
 };
 
 function windowMessageListener(message: MessageEvent) {
-    if (message.data?.source && message.data?.source === "sb-reset-media-session-link") {
-        resetLastArtworkSrc();
-    } else if (message.data?.source && message.data?.source === sourceId) {
+    if (!message.data?.source) {
+        return;
+    }
+    if (message.data?.source === sourceId) {
         if (message.data?.type === "getBvID") {
             sendMessage({ type: "returnBvID", id: message.data.id, bvID: window?.__INITIAL_STATE__?.bvid });
         }
