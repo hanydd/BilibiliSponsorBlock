f68b01cba3fa6f7aec3c6b7059b669dd382f9084
HanYaodong <hanyaodongcn@gmail.com>
2024-08-09 16:57:25 +0800

    Update dependencies

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -2,6 +2,7 @@ import SBObject from "./config";
 declare global {
     interface Window {
         SB: typeof SBObject;
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         __INITIAL_STATE__?: any;
     }
 }
