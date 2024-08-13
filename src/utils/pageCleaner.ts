d50fa7aa5e13baa009c4aa4abcd80e76c4509e83
HanYaodong <hanyaodongcn@gmail.com>
2024-08-13 21:27:48 +0800

    Add port video elements to cleanup

--- src/utils/pageCleaner.ts
+++ src/utils/pageCleaner.ts
@@ -1,9 +1,23 @@
+const bsbElements = [
+    "#categoryPillParent",
+    ".playerButton",
+    ".sponsorThumbnailLabel",
+    "#submissionNoticeContainer",
+    ".sponsorSkipNoticeContainer",
+    "#sponsorBlockPopupContainer",
+    ".skipButtonControlBarContainer",
+    "#previewbar",
+    "#shadowPreviewbar",
+    "#bsbPortButton",
+    "#bsbDescriptionContainer",
+];
+
+const bsbElementsSelector = bsbElements.join(", ");
+
 export function cleanPage() {
     // For live-updates
     if (document.readyState === "complete") {
-        for (const element of document.querySelectorAll(
-            "#categoryPillParent, .playerButton, .sponsorThumbnailLabel, #submissionNoticeContainer, .sponsorSkipNoticeContainer, #sponsorBlockPopupContainer, .skipButtonControlBarContainer, #previewbar, #shadowPreviewbar"
-        )) {
+        for (const element of document.querySelectorAll(bsbElementsSelector)) {
             element.remove();
         }
     }
