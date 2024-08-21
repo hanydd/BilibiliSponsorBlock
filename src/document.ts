a1c7d8c3e636e4eed18e40cf70b037f5e499547f
hanyd <hanyaodongcn@gmail.com>
2024-08-21 14:05:32 +0800

    Add window object get fallback

--- src/document.ts
+++ src/document.ts
@@ -21,11 +21,11 @@ function windowMessageListener(message: MessageEvent) {
         if (data.type === "getBvID") {
             sendMessageToContent(data, window?.__INITIAL_STATE__?.bvid);
         } else if (data.type === "getFrameRate") {
-            const currentQuality = window.__playinfo__.data.quality;
-            const frameRate = window.__playinfo__.data.dash.video.filter((v) => v.id === currentQuality)[0].frameRate;
+            const currentQuality = window?.__playinfo__?.data?.quality;
+            const frameRate = window?.__playinfo__?.data?.dash?.video.filter((v) => v.id === currentQuality)[0]?.frameRate;
             sendMessageToContent(data, frameRate);
         } else if (data.type === "getChannelID") {
-            sendMessageToContent(data, window.__INITIAL_STATE__.upInfo.mid);
+            sendMessageToContent(data, window?.__INITIAL_STATE__?.upInfo?.mid);
         }
     }
 }
