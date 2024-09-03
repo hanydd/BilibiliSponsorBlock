c09356d6d7771f5034422235fd5f7a96fc9667c2
hanyd <hanyd@bsbsb.top>
2024-09-03 22:16:47 +0800

    Change button container to be rendered when creating buttons

--- src/render/PlayerButton.tsx
+++ src/render/PlayerButton.tsx
@@ -3,6 +3,7 @@ import { createRoot, Root } from "react-dom/client";
 import { PlayerButtonGroupComponent } from "../components/PlayerButtonComponent";
 import Config from "../config";
 import { AnimationUtils } from "../utils/animationUtils";
+import { waitForElement } from "../utils/dom";
 import { waitFor } from "../utils/index";
 import { getControls } from "../utils/pageUtils";
 
@@ -13,6 +14,12 @@ export class PlayerButton {
     root: Root;
     container: HTMLElement;
 
+    startSegmentCallback: () => void;
+    cancelSegmentCallback: () => void;
+    deleteCallback: () => void;
+    submitCallback: () => void;
+    infoCallback: () => void;
+
     constructor(
         startSegmentCallback: () => void,
         cancelSegmentCallback: () => void,
@@ -20,69 +27,88 @@ export class PlayerButton {
         submitCallback: () => void,
         infoCallback: () => void
     ) {
-        this.ref = React.createRef();
+        this.ref = React.createRef<PlayerButtonGroupComponent>();
         const existingContainer = document.getElementById(containerId);
         if (existingContainer) {
-            this.container = existingContainer;
-            return;
+            existingContainer.remove();
         }
-
-        this.container = document.createElement("div");
-        this.container.id = containerId;
-        this.container.style.display = "contents";
-        this.root = createRoot(this.container);
-        this.root.render(
-            <PlayerButtonGroupComponent
-                ref={this.ref}
-                startSegmentCallback={startSegmentCallback}
-                cancelSegmentCallback={cancelSegmentCallback}
-                deleteCallback={deleteCallback}
-                submitCallback={submitCallback}
-                infoCallback={infoCallback}
-            />
-        );
+        this.startSegmentCallback = startSegmentCallback;
+        this.cancelSegmentCallback = cancelSegmentCallback;
+        this.deleteCallback = deleteCallback;
+        this.submitCallback = submitCallback;
+        this.infoCallback = infoCallback;
     }
 
     public async createButtons(): Promise<
         Record<string, { button: HTMLButtonElement; image: HTMLImageElement; setupListener: boolean }>
     > {
-        const controlsContainer = await waitFor(getControls, 10000).catch();
-        await waitFor(() => getControls().childElementCount > 4); // wait for controls buttons to be loaded
-
-        controlsContainer.prepend(this.container);
-
-        const playerButtons = {
-            startSegment: {
-                button: document.getElementById("startSegmentButton") as HTMLButtonElement,
-                image: document.getElementById("startSegmentImage") as HTMLImageElement,
-                setupListener: false,
-            },
-            cancelSegment: {
-                button: document.getElementById("cancelSegmentButton") as HTMLButtonElement,
-                image: document.getElementById("cancelSegmentImage") as HTMLImageElement,
-                setupListener: false,
-            },
-            delete: {
-                button: document.getElementById("deleteButton") as HTMLButtonElement,
-                image: document.getElementById("deleteImage") as HTMLImageElement,
-                setupListener: false,
-            },
-            submit: {
-                button: document.getElementById("submitButton") as HTMLButtonElement,
-                image: document.getElementById("submitImage") as HTMLImageElement,
-                setupListener: false,
-            },
-            info: {
-                button: document.getElementById("infoButton") as HTMLButtonElement,
-                image: document.getElementById("infoImage") as HTMLImageElement,
-                setupListener: false,
-            },
-        };
-
-        if (Config.config.autoHideInfoButton) {
-            AnimationUtils.setupAutoHideAnimation(playerButtons.info.button, controlsContainer);
+        const controlsContainer = await waitForElement(".bpx-player-control-bottom-right").catch(() => null);
+        console.log("controlsContainer", controlsContainer);
+        if (!controlsContainer) {
+            console.log("Could not find control button containers");
+            return null;
         }
 
-        return playerButtons;
+        // wait for controls buttons to be loaded
+        await waitFor(() => getControls().childElementCount > 4).catch(() => {
+            console.log("Get control chilren time out");
+        });
+
+        if (!this.container) {
+            this.container = document.createElement("div");
+            this.container.id = containerId;
+            this.container.style.display = "contents";
+            this.root = createRoot(this.container);
+            this.root.render(
+                <PlayerButtonGroupComponent
+                    ref={this.ref}
+                    startSegmentCallback={this.startSegmentCallback}
+                    cancelSegmentCallback={this.cancelSegmentCallback}
+                    deleteCallback={this.deleteCallback}
+                    submitCallback={this.submitCallback}
+                    infoCallback={this.infoCallback}
+                />
+            );
+            controlsContainer.prepend(this.container);
+        }
+
+        // wait a tick for React to render the buttons
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                const playerButtons = {
+                    startSegment: {
+                        button: document.getElementById("startSegmentButton") as HTMLButtonElement,
+                        image: document.getElementById("startSegmentImage") as HTMLImageElement,
+                        setupListener: false,
+                    },
+                    cancelSegment: {
+                        button: document.getElementById("cancelSegmentButton") as HTMLButtonElement,
+                        image: document.getElementById("cancelSegmentImage") as HTMLImageElement,
+                        setupListener: false,
+                    },
+                    delete: {
+                        button: document.getElementById("deleteButton") as HTMLButtonElement,
+                        image: document.getElementById("deleteImage") as HTMLImageElement,
+                        setupListener: false,
+                    },
+                    submit: {
+                        button: document.getElementById("submitButton") as HTMLButtonElement,
+                        image: document.getElementById("submitImage") as HTMLImageElement,
+                        setupListener: false,
+                    },
+                    info: {
+                        button: document.getElementById("infoButton") as HTMLButtonElement,
+                        image: document.getElementById("infoImage") as HTMLImageElement,
+                        setupListener: false,
+                    },
+                };
+
+                if (Config.config.autoHideInfoButton) {
+                    AnimationUtils.setupAutoHideAnimation(playerButtons.info.button, controlsContainer);
+                }
+
+                resolve(playerButtons);
+            }, 10);
+        });
     }
 }
