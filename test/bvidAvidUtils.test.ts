ef412b0921923f464bf701dbc1c539771b87d6a8
xiaoxuan010 <2592053474@qq.com>
2025-01-23 17:02:17 +0800

    fix(test): fix test case for bvid2avid
    修复了BV号转AV号的测试

--- test/bvidAvidUtils.test.ts
+++ test/bvidAvidUtils.test.ts
@@ -1,12 +1,13 @@
-import { CalculateAvidToBvid } from "../src/utils/bvidAvidUtils";
-const testCases: [number | string, string][] = [
-    [2, "BV1xx411c7mD"],
-    ["170001", "BV17x411w7KC"],
-    ["455017605", "BV1Q541167Qg"],
-    [882584971, "BV1mK4y1C7Bz"],
-    ["80433022", "BV1GJ411x7h7"],
-    [713984017, "BV18X4y1N7Yh"],
-    [113864347752328, "BV1CDwbeEE46"],
+import { BVID } from "../src/types";
+import { CalculateAvidToBvid, CalculateBvidToAvid } from "../src/utils/bvidAvidUtils";
+const testCases: [number | string, BVID][] = [
+    [2, "BV1xx411c7mD" as BVID],
+    ["170001", "BV17x411w7KC" as BVID],
+    ["455017605", "BV1Q541167Qg" as BVID],
+    [882584971, "BV1mK4y1C7Bz" as BVID],
+    ["80433022", "BV1GJ411x7h7" as BVID],
+    [713984017, "BV18X4y1N7Yh" as BVID],
+    [113864347752328, "BV1CDwbeEE46" as BVID],
 ];
 
 describe("AV号转BV号", () => {
@@ -20,7 +21,7 @@ describe("AV号转BV号", () => {
 describe("BV号转AV号", () => {
     testCases.forEach(([avid, bvid]) => {
         test(`${bvid} -> av${avid}`, () => {
-            expect(CalculateAvidToBvid(avid)).toBe(bvid);
+            expect(CalculateBvidToAvid(bvid)).toBe(Number(avid));
         });
     });
 });
